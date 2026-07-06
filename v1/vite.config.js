1e521a43965b3df6009ea0631949aabbb6c3e633
bogdan <bogdan.draghici@flowx.ai>
2026-07-06 12:02:38 +0300

    feat: insurance industry page

--- v1/vite.config.js
+++ v1/vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
         main: fileURLToPath(new URL('./index.html', import.meta.url)),
         agents: fileURLToPath(new URL('./agents.html', import.meta.url)),
         banking: fileURLToPath(new URL('./banking.html', import.meta.url)),
+        insurance: fileURLToPath(new URL('./insurance.html', import.meta.url)),
         about: fileURLToPath(new URL('./about.html', import.meta.url)),
         resources: fileURLToPath(new URL('./resources.html', import.meta.url)),
         blogflowx6: fileURLToPath(new URL('./blog-flowx-6.html', import.meta.url)),
